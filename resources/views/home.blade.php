2ad5954b8cd383f599b2e1cd46c8c24ca4316be7
VangelisGiannakosian <vgiannakosian@pavla.gr>
2020-03-29 14:07:26 +0300

    created simple controller created layout.blade.php created first set of routes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layout')
+
+
+@section('content')
+
+    <h1>Welcome to Laravel!</h1>
+    <p>This is the content of the main page!</p>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/','HomeController@home')->name('home');
+Route::get('contact','HomeController@contact')->name('contact');
+Route::get('/blog-post/{id}/{welcome?}','HomeController@blogPost')->name('blog-post');
