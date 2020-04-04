898fbc225b33e7ffe66dec7426f0d08b7de37039
VangelisGiannakosian <vgiannakosian@pavla.gr>
2020-04-04 15:18:13 +0300

    added validation messages to edit.blade.php

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -11,10 +11,22 @@
             <input type="text" name="title" id="title" value="{{ $post->title }}">
         </p>
 
+
+        @error('title')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+
         <p>
             <label for="content">Content</label>
             <textarea name="content" id="content" cols="30" rows="10" placeholder="Write the content here...">{{ $post->content }}</textarea>
         </p>
+
+
+        @error('content')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
 
         <button type="submit">Update!</button>
 
