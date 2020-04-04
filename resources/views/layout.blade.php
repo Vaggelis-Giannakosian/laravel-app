2e892d1960886068cfecffa2eb3e6ebf02dcc6db
VangelisGiannakosian <vgiannakosian@pavla.gr>
2020-04-04 18:20:43 +0300

    added bootstrap and vue.js setup bundled static assets using laravel mix added assets to layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @yield('css')
 </head>
 <body>
@@ -26,5 +27,6 @@
 @yield('content')
 
 @yield('js')
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
