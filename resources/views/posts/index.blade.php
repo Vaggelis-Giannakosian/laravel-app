2a12950668c56c872f1f3a9823dec4d23195bb2f
VangelisGiannakosian <vgiannakosian@pavla.gr>
2020-04-04 18:55:33 +0300

    minor changes to index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,18 +5,17 @@
 @section('content')
 
     @forelse($posts as $post)
-        <div>
+        <div class="mb-4">
           <h3>
               <a href="{{ route('posts.show',['post'=>$post->id]) }}" >{{ $post->title }}</a>
           </h3>
 
-            <a class="btn btn-primary" href="{{ route('posts.edit',['post'=>$post->id]) }}" >Edit</a>
+            <a class="btn btn-primary btn-sm" href="{{ route('posts.edit',['post'=>$post->id]) }}" >Edit</a>
             <form class="fm-inline" action="{{ route('posts.destroy',['post'=>$post->id]) }}" method="POST">
                 @method('DELETE')
                 @csrf
-                <button type="submit" class="btn btn-primary">DELETE</button>
+                <button type="submit" class="btn btn-primary btn-sm">DELETE</button>
             </form>
-            <p>{{ $post->content }}</p>
         </div>
     @empty
         <p>No blog posts yet!</p>
