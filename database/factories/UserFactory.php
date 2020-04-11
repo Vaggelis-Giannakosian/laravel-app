50d376d04de859a682b7a74a2a9d6d1d667f8933
VangelisGiannakosian <vgiannakosian@pavla.gr>
2020-04-11 18:52:27 +0300

    added is_admin to users table adjusted users factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'is_admin' => false
     ];
 });
 
@@ -33,5 +34,6 @@ $factory->state(User::class, 'vag',function(Faker $faker){
         'name' => 'Vagelis Giannakosian',
         'email' => 'e.giannakosian@gmail.com',
         'password' => '$2y$10$yQan4fTAJtb.U7ZEv7ZdKOxKB7QiPiw.qZH6qi4wsv/.TqmKUxG6.', // 123456789
+        'is_admin' => true
     ];
 });
